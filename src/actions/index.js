8cf06f99e55384e467f4e6e9a8d2f1a21500fce8
daria marczak <marczak.ria@gmail.com>
2018-06-23 14:50:04 +0200

    Added fetch action to Redux

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import { FETCH_PHOTOS } from "./types";
+import axios from "axios";
+
+export function fetchPhotos() {
+  const response = axios.get("https://dog.ceo/api/breeds/list/all");
+
+  return {
+    type: FETCH_PHOTOS,
+    payload: response
+  };
+}
