2cdd819754ef7facc8deee22600134148f4aa9c6
daria marczak <marczak.ria@gmail.com>
2018-06-23 14:38:36 +0200

    First commit

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SinglePhoto from "./SinglePhoto";
+
+const PhotoGrid = () => {
+  return (
+    <div>I'm the PhotoGrid
+      <SinglePhoto />
+    </div>
+  );
+}
+
+export default PhotoGrid;
---
+++ src/components/SinglePhoto.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SinglePhoto = () => {
+  return (
+    <p>I'm a photo!</p>
+  );
+}
+
+export default SinglePhoto;
